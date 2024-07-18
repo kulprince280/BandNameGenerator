4ef349be55eec9a25d20bbf4a8475efc1e62c444
Princewill Okoli <35287091-kulprince280@users.noreply.replit.com>
2024-07-18 18:41:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to Brand name generator")
+city = input("What is the name of your city?\n")
+pet = input("What is the name of your pet?\n")
+print("Your brand name could be " + city + " " + pet)
